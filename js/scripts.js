d02068564f63e2da7dd80cced2d8cea430c57ca1
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-11-20 22:54:38 -0600

    add variables to the survey function so that they can be returned.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-  $("form").submit(function() {
-    alert("Thanks for submitting your survey responses!");
-    var surveyResponse=""
-    console.log(surveyResponse);
+  $("form#user-survey").submit(function(event) {
+    // alert("Thanks for submitting your survey responses!");
+    var firstName = $("#name").val();
+    var favFood = $("#food").val();
+    var favMusic = $('input[name="optionsRadios"]').val();
+    var favColor = $("#color").val();
+
+    alert("Hello " + firstName + ". I'm glad you also like to eat " + favFood + ". I'm not a huge fan of " + favMusic + " music myself, but I won't judge." + favColor + " is a pretty cool color!");
+
     event.preventDefault();
   })
 })
